18461fcd7a13dc5815d72e6d904026da989d0a17
greengay <vert.p@protonmail.com>
2022-08-10 20:04:05 -0400

    intial commit

---
+++ center.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+
+# this is a script from a walkthrough youtube tutorial that I am doing to learn
+
+from gimpfu import *
+
+def center_elements(image, drawable):
+    return
+
+register(
+    "Center",
+    "Center visible layers in the file.",
+    "Center elements.",
+    "Alex from youtube",
+    "Alex Zorzella",
+    "2021",
+    "<Image>/Tools/Center_Elements",
+    "*",
+    [
+
+    ],
+    [],
+    center_elements)
+
+main()
